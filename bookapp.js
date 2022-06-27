868645b6075f1b3feb69b6b14020a8daa2f87718
karlavdelgadof <karlavdelgadof@gmail.com>
2022-06-27 13:51:27 -0500

    Add first functions for the book app

---
+++ bookapp.js
@@ -0,0 +1,16 @@
+const bookArr = [];
+
+function createBook(book) {
+  const book = {title, author};
+  this.title = document.getElementById('title').value;
+  this.author = document.getElementById('author').value;
+  // const id = 0;
+  // this.id = id + 1;
+  bookArr.push(book);
+};
+
+function removeBook(book) {
+  bookArr.filter( (book) => {
+    
+  })
+}
